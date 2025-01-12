15285ec0367a55515c5491fd07cc77d44f5ac9cb
rkbarney <rkbarney@gmail.com>
2025-01-11 18:25:23 -0800

    Update: Replace Registry iframe with direct link

--- src/pages/Registry.tsx
+++ src/pages/Registry.tsx
@@ -1,4 +1,4 @@
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Typography, Box, Button } from '@mui/material';
 
 const Registry = () => {
   return (
@@ -14,17 +14,37 @@ const Registry = () => {
         >
           Registry
         </Typography>
-        <Box 
-          component="iframe"
-          src="https://www.myregistry.com/wedding-registry/richard-barney-and-emily-sheehy-wilkins-/4650913"
+        
+        <Typography
+          variant="h6"
           sx={{
-            width: '100%',
-            height: '800px',
-            border: 'none',
-            mt: 2,
-            backgroundColor: 'transparent'
+            mb: 4,
+            textAlign: 'center',
+            maxWidth: 600,
+            mx: 'auto',
           }}
-        />
+        >
+          Your presence at our wedding is the greatest gift of all. However, if
+          you wish to honor us with a gift, we have created a registry at MyRegistry.com
+        </Typography>
+
+        <Button
+          variant="contained"
+          color="primary"
+          href="https://www.myregistry.com/wedding-registry/richard-barney-and-emily-sheehy-wilkins-/4650913"
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{
+            fontSize: '1.1rem',
+            padding: '12px 24px',
+            backgroundColor: 'primary.main',
+            '&:hover': {
+              backgroundColor: 'primary.dark',
+            },
+          }}
+        >
+          View Our Registry
+        </Button>
       </Box>
     </Container>
   );
