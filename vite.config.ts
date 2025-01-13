dab01879292d02c0f77209d6882a077875026042
rkbarney <rkbarney@gmail.com>
2025-01-13 15:56:12 -0800

    Update vite config for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/wedding-website/' : '/',
+export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.png', '**/*.jpeg']
-}))
+})
