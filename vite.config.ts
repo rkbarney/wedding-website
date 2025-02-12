7563bb1a9473895b0f376c7e0bd1ca7c041bc9ce
rkbarney <rkbarney@gmail.com>
2025-02-12 18:22:57 -0500

    Fix base URL for local development

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.png', '**/*.jpeg'],
-  base: '/wedding-website/',
+  base: command === 'serve' ? '/' : '/wedding-website/',
   build: {
     outDir: 'dist'
   }
-})
+}))
